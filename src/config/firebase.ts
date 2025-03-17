ce83c875d6d5918c489c6e22e4c7c442f8b74a3f
Yvesmorel <hackeryves842@gmail.com>
2025-03-17 10:16:45 +0000

    first commit

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../../firebaseService.json");
+const serviceAccount = require("../../firebase/firebaseService.json");
 
 const firebase = require("firebase/app");
 
